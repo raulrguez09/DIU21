9f421ab4259219b4afd625daebf573518f0901d7
Rullencastro <rullencastro@gmail.com>
2021-03-15 18:07:06 +0100

    segundo journey

--- P1/UX-DIU-Toolkit-master/Journey.js
+++ P1/UX-DIU-Toolkit-master/Journey.js
@@ -92,54 +92,54 @@ angular.module("angular", [])
                 /*************************************/
                 
 				Id: 1,
-				Name: "Monica Suarez",
+				Name: "Mei Ling",
                 Photo: "woman.png",
                 
 				 /*** PASO #1: INSPIRACION ***/ 
-                goal1: "Quiere preparar un viaje con su familia para Verano, tiene sólo 15 dias libres",
+                goal1: "Realizar un viaje gastronómico por Granada, para descubrir los sabores del Meditarráneo",
                 touch1: "agenda",
                 feel1: "5",
-                con1: "Quiere ir a un pais exotico pero que tenga atracciones para niños pequeños",
-                ima1: "cartoon-going.png",
+                con1: "Necesita esperar a un periodo de vacaciones para poder realizarlo sin preocupaciones",
+                ima1: "cartoon-writing.png",
                 
                 /*** PASO #2: DECICION ***/ 
                 goal2: "Ir a una agencia de viajes, y decirle sus preferencias y planes",
                 touch2: "Servicio (agencia)",
-                feel2: "4",
-                con2: "Tiene que desplazarse a agencia, explica su intenciones, le llamaran porque no hay nada interesante",
+                feel2: "3",
+                con2: "Tiene que desplazarse a agencia, explica su intenciones, no tienen ningún viaje acorde a lo que pide, pero dice que le llamaran si aparece algo interesante",
                 ima2: "cartoon-teamthinking.png",
                 
                 /*** PASO #3: ACTUA ***/ 
                 
-                goal3: "Le llaman a los pocos días con un viaje que no le convence",
+                goal3: "La agencia no le da una solucion a lo que busca y decide buscarlo ella misma",
                 touch3: "Móvil (llamada)",
                 feel3: "2",
-                con3: "Piensa que ha perdido el tiempo",
+                con3: "Ni ella ni sus padres han viajado nunca a España y no sabe que sitios mirar",
                 ima3: "cartoon-phoningangry.png",
                 
                 /*** PASO #4: OBSERVA ***/ 
                 
-                goal4: "Busca una oferta en hoteles cerca de playa y con parque atracciones",
+                goal4: "El padre tiene un amigo español, y este les recomienda una página web",
                 touch4: "Móvil (webapp)",
-                feel4: "2",
-                con4: "No hay mucha información del alojamiento ni de lo que hay alrededor, aunque el precio está bien, va por la calle por lo que está incómoda",
+                feel4: "4",
+                con4: "La página web no está adaptada a su idioma, y no entiende mucho de español",
                 ima4: "cartoon-phone-street.png",
                 
                  /*** PASO #5: ANALIZA ***/ 
                 
-                goal5: "Reserva a traves de la aplicación ",
+                goal5: "Con la ayuda de un traductor web, consigue mirar los tours gastronómicos disponibles",
                 touch5: "Móvil (webapp)",
-                feel5: "3",
-                con5: "Le pide muchos datos y le resulta incómodo completar formulario",
+                feel5: "1",
+                con5: "Tiene problemas con la estructura de la página porque no encuentra fácilmente lo que busca",
                 ima5: "cartoon-phone-sitting.png",
 
                 
                 /*** PASO #6: CONCLUSION ***/ 
                 
-                goal6: "Consiguie reservar para vacaciones pero no era lo que tenía en mente",
+                goal6: "Consigue una ruta que se adapte a lo que busca",
                 touch6: "Ordenador (reserva OK)",
-                feel6: "2",
-                con6: "Tendrá que buscar más información del lugar para ver que actividades ofrece y donde aparacar!",
+                feel6: "3",
+                con6: "Puesto que no está adaptada a su idioma, no está muy segura de lo que ha encontrado",
                 ima6: "cartoon-PChard.png",
                 
                 
